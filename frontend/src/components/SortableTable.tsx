bd0c75f556feeb2b4dbfe09162be9e5dae2f0ba3
ZHENGLI <Jokerxtermlxc@gmail.com>
2024-09-30 02:56:42 +0800

    update sortable table components

--- frontend/src/components/SortableTable.tsx
+++ frontend/src/components/SortableTable.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import styles from "../styles/SortableTable.module.scss";
+import { useRouter } from "next/router";
 
 interface SortableTableProps {
   headers: { key: string; label: string }[];
   data: any[];
+  renderOperation?: (row: any) => React.ReactNode; // 可选的渲染函数
 }
 
-const SortableTable: React.FC<SortableTableProps> = ({ headers, data }) => {
+const SortableTable: React.FC<SortableTableProps> = ({ headers, data, renderOperation }) => {
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
   const [sortKey, setSortKey] = useState<string | null>(null);
   const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
 
@@ -16,12 +21,19 @@ const SortableTable: React.FC<SortableTableProps> = ({ headers, data }) => {
     setSortDirection(direction);
   };
 
+  const handleClick = async (id: any) => {
+    router.push({
+      pathname: '/articles/article-detail',
+      query: { id }
+    });
+  };
+
   const sortedData = sortKey
     ? [...data].sort((a, b) => {
-        if (a[sortKey] < b[sortKey]) return sortDirection === "asc" ? -1 : 1;
-        if (a[sortKey] > b[sortKey]) return sortDirection === "asc" ? 1 : -1;
-        return 0;
-      })
+      if (a[sortKey] < b[sortKey]) return sortDirection === "asc" ? -1 : 1;
+      if (a[sortKey] > b[sortKey]) return sortDirection === "asc" ? 1 : -1;
+      return 0;
+    })
     : data;
 
   return (
@@ -33,14 +45,20 @@ const SortableTable: React.FC<SortableTableProps> = ({ headers, data }) => {
               {header.label} {sortKey === header.key ? (sortDirection === "asc" ? "↑" : "↓") : ""}
             </th>
           ))}
+          <th>Operation</th>
         </tr>
       </thead>
       <tbody>
         {sortedData.map((row, i) => (
           <tr key={i}>
-            {headers.map((header) => (
-              <td key={header.key}>{row[header.key]}</td>
-            ))}
+            {
+              headers.map((header) => (
+                <td key={header.key}>{row[header.key]}</td>
+              ))
+            }
+            {renderOperation ? renderOperation(row) : (
+              <td><button onClick={() => handleClick(row.id)}>Detail</button></td>
+            )}
           </tr>
         ))}
       </tbody>
