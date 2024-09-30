d46ff8b983071e718f6a0bf8c8045f6b3174bd65
ZHENGLI <Jokerxtermlxc@gmail.com>
2024-10-01 00:37:02 +0800

    Configure the front end to use the dotenv package.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout"; // Import Layout
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
