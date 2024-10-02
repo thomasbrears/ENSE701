574d9ba4cbd74ea59ef808046eb9275ad31bc684
Onafanewlevel <rosemicluvin@icloud.com>
2024-10-03 04:49:47 +1300

    Added evidence summary to Schema and updated relavent api's.

--- backend/models/Article.js
+++ backend/models/Article.js
@@ -5,9 +5,9 @@ const articleSchema = new mongoose.Schema({
   user_email: { type: String, required: true },
   title: { type: String, required: true },
   authors: [String],
-  journal: String, 
-  se_practice: String, 
-  research_type: String, 
+  journal: String,
+  se_practice: String,
+  research_type: String,
   publication_year: Number,
   volume: String,
   number: String,
@@ -16,13 +16,14 @@ const articleSchema = new mongoose.Schema({
   summary: String,
   claim: String,
   evidence: { type: String, default: null }, // Analyst will add the evidence later
+  evidence_summary: { type: String, enum: ['weak', 'moderate', 'strong'], default: null }, // Analyst will add the evidence summary later
   linked_discussion: String,
   status: { type: String, enum: ['pending', 'approved_by_moderator', 'published', 'rejected'], default: 'pending' },
   moderation_notes: String,
-  analysis_notes: String,
+  analysis_notes: { type: String, default: null }, // Analysis will add the analysis notes later
   submitted_at: { type: Date, default: Date.now }, // Timestamp of submission
   moderated_at: Date,  // Timestamp when moderated
-  analyzed_at: Date,  // Timestamp when analyzed
+  analyzed_at: { type: Date, default: Date.now},  // Timestamp when analyzed
 });
 
 const Article = mongoose.model('Article', articleSchema);
